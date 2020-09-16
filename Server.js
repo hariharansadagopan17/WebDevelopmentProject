9defa4e0b96b5fd2897f0a81ca3580d25b028df8
Abarajith Sadagopan <hariharansadagopan17@gmail.com>
2020-09-16 23:38:21 +0530

    inital commit

---
+++ Server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// define routes here..
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/MyProfile.html');
+});
+
+app.get('/HomePage.html',function(req,res){
+
+    res.sendFile(__dirname + '/HomePage.html');
+});
+
+
+
+var server = app.listen(8083, function () {
+    console.log('Node server is running..');
+});
